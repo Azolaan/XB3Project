bf4caec75bea88b6edce1a978d704288423d6a74
Gabriel <yug13@mcmaster.ca>
2018-03-14 18:50:16 -0400

    added sample main

---
+++ XB3Pro/src/main_func.java
@@ -0,0 +1,15 @@
+package lapages;
+import java.sql.*;
+
+public class main_func {
+	public static void main(String[] args) {
+		System.out.println("Enter business tag of choice : ");
+		String user_tag = args[0];
+		System.out.println("Enter business name (optional))");
+		String user_name = args[1];
+		System.out.println("Enter current location (address)");
+		String user_loc = args[2];
+		
+		csv_filter();
+	}
+}
