7fcdfdd37b459cd921f524e7c5858d13900d4268
yug13 <yug13@mcmaster.ca>
2018-03-15 12:54:49 -0400

    added distanceCalc into ADT

--- XB3Pro/src/LA_pages/business_coord.java
+++ XB3Pro/src/LA_pages/business_coord.java
@@ -12,6 +12,7 @@ public class business_coord {
 	private double bus_lat;
 	private double bus_long;
 	public String user_loc;
+	public final static double AVERAGE_RAD_EARTH_KM = 6371.0;
 	
 	public business_coord(String name, String number, String location, String tag, double lat, double lon) {
 		this.bus_name = name;
@@ -61,4 +62,14 @@ public class business_coord {
 		System.out.println("Distance : " + Sort_coord.calculateDist(user_lat, user_long, this.bus_lat, this.bus_long)+ " km.");		
 	}
 	
+	public double calculateDist(double user_lat, double user_long){
+		double latDistance = Math.toRadians(userLat - this.bus_lat);
+        double lngDistance = Math.toRadians(userLng - this.bus_long);
+        double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
+        + Math.cos(Math.toRadians(userLat)) * Math.cos(Math.toRadians(this.bus_lat))
+        * Math.sin(lngDistance / 2) * Math.sin(lngDistance / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        double dist = ((double)Math.round((AVERAGE_RAD_EARTH_KM * c * 100000d)))/100000d; 
+        return  dist;
+	}
 }
