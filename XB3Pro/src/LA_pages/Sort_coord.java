f4a1fa5ecc5dda4f408cb75e2928178998dc853f
Gabriel <yug13@mcmaster.ca>
2018-03-15 12:31:40 -0400

    changes

--- XB3Pro/src/LA_pages/Sort_coord.java
+++ XB3Pro/src/LA_pages/Sort_coord.java
@@ -50,18 +50,12 @@ public class Sort_coord {
 	}
 	    
 	public static double calculateDist(double userLat, double userLng, double venueLat, double venueLng) {
-
-	double latDistance = Math.toRadians(userLat - venueLat);
-	double lngDistance = Math.toRadians(userLng - venueLng);
-
-	double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
-	+ Math.cos(Math.toRadians(userLat)) * Math.cos(Math.toRadians(venueLat))
-	* Math.sin(lngDistance / 2) * Math.sin(lngDistance / 2);
-
-	double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-
-	return AVERAGE_RADIUS_OF_EARTH_KM * c/100.0;
+		double latDistance = Math.toRadians(userLat - venueLat);
+		double lngDistance = Math.toRadians(userLng - venueLng);
+		double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
+				+ Math.cos(Math.toRadians(userLat)) * Math.cos(Math.toRadians(venueLat))
+				* Math.sin(lngDistance / 2) * Math.sin(lngDistance / 2);
+		double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+		return AVERAGE_RADIUS_OF_EARTH_KM * c/100.0;
 	}
-	
-	
 }
